6a593235c959a94daf6e28d8c6d557fe22045728
HeyItsMeBJ <bhupeshjain3221@gmail.com>
2024-04-27 16:04:05 +0530

    add comments and deleteVideo & togglePublishStatus hitpoints to the video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,6 +9,8 @@ import { Video } from "../models/video.models.js";
 import { ApiRes } from "../utils/ApiRes.js";
 import mongoose from "mongoose";
 import { extractPublicId } from "cloudinary-build-url";
+import { Like } from "../models/like.models.js";
+import { Comment } from "../models/comment.models.js";
 
 const getAllVideos = asyncHandler(async (req, res, next) => {
   const { page = 1, limit = 10, userId, query, shortBy, shortType } = req.query;
@@ -21,6 +23,7 @@ const publishVideo = asyncHandler(async (req, res) => {
   const { title, discription } = req.body;
   const { videoFile, thumbnail } = req.files;
 
+  //get file local paths
   const videoFilePath = videoFile ? videoFile[0]?.path : "";
   const thumbnailPath = thumbnail ? thumbnail[0]?.path : "";
 
@@ -30,6 +33,7 @@ const publishVideo = asyncHandler(async (req, res) => {
     throw new ApiError("File not found, upload again", 500);
   }
 
+  // check title & discription
   if (!title?.trim() || !discription?.trim()) {
     if (videoFilePath) fs.unlink(videoFilePath);
     if (thumbnailPath) fs.unlink(thumbnailPath);
@@ -37,8 +41,9 @@ const publishVideo = asyncHandler(async (req, res) => {
   }
 
   const userId = req.user?._id;
-  if (!userId) throw new ApiError("Id not found", 500);
+  if (!userId) throw new ApiError("user Id not found", 500);
 
+  // upload video and thumbnail to cloudinary
   const uploadedVideoFile = await uploadCloudinaryFile(videoFilePath);
   if (!uploadedVideoFile) {
     fs.unlink(thumbnailPath);
@@ -51,6 +56,7 @@ const publishVideo = asyncHandler(async (req, res) => {
     throw new ApiError("upload failed", 500);
   }
 
+  // make video document
   const video = await Video.create({
     videoFile: uploadedVideoFile.url,
     thumbnail: uploadedThumbnail.url,
@@ -59,18 +65,17 @@ const publishVideo = asyncHandler(async (req, res) => {
     discription,
     duration: uploadedVideoFile.duration,
   });
-
   if (!video) throw new ApiError("Video not created", 500);
 
+  // return
   return res.status(201).json(new ApiRes(201, "Video created", video));
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
-  //TODO: get video by id
-
   const { videoId } = req.params;
   if (!videoId) throw new ApiError("Video id is required", 400);
 
+  // aggreation pipeline to get video and add likes and views
   const video = await Video.aggregate([
     {
       // find video
@@ -161,35 +166,41 @@ const getVideoById = asyncHandler(async (req, res) => {
       },
     },
   ]);
-
   if (!video) throw new ApiError("Video not found", 404);
+
+  // return
   return res.status(200).json(new ApiRes(200, "Video found", video[0]));
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
   //  update video details like title, description, thumbnail
-
   const { videoId } = req.params;
   const { title, description } = req.body;
   const thumbnailFile = req.file;
 
   if (!thumbnailFile) throw new ApiError("Thumbnail not Provided", 400);
   const thumbnailPath = thumbnailFile.path;
+
+  // check if title and description is provided
   if (!title || !description) {
     fs.unlink(thumbnailPath);
     throw new ApiError("Title or description not provided", 400);
   }
-  
 
+  // check video
   const video = await Video.findById(videoId);
   if (!video) {
     fs.unlink(thumbnailPath);
     throw new ApiError("Video not found", 404);
   }
-  if(video.owner.toString()!=req.user?._id){
-    fs.unlink(thumbnailPath)
+
+  // check auth of video
+  if (video.owner.toString() != req.user?._id) {
+    fs.unlink(thumbnailPath);
     throw new ApiError("User not authenticated to this video", 400);
   }
+
+  // delete old thumbnail and add new one
   const oldThumbnailCloudinaryPublicId = extractPublicId(video.url);
   const deleteOldThumbnail = await deleteCloudinaryFile(
     oldThumbnailCloudinaryPublicId
@@ -203,6 +214,7 @@ const updateVideo = asyncHandler(async (req, res) => {
     throw new ApiError("Thumbnail upload problem", 500);
   }
 
+  // update video
   const updatedVideo = await Video.findByIdAndUpdate(
     videoId,
     {
@@ -219,6 +231,7 @@ const updateVideo = asyncHandler(async (req, res) => {
     throw new ApiError("Video update problem", 500);
   }
 
+  //return
   return res
     .status(201)
     .json(new ApiRes(201, "video updated successfully", updateVideo));
@@ -226,11 +239,70 @@ const updateVideo = asyncHandler(async (req, res) => {
 
 const deleteVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
-  //TODO: delete video
+  if (!videoId) throw new ApiError("Video Id not found", 404);
+
+  // check video
+  const video = await Video.findById(videoId);
+  if (!video) throw new ApiError("Video not found", 404);
+
+  // check auth
+  if (video.owner.toString() != req.user?._id)
+    throw new ApiError("User not authenticated for this video", 400);
+
+  //delete likes of video
+  const isLikesDeleted = await Like.deleteMany({ video: ObjectId(video?._id) });
+  if (!isLikesDeleted?.ok) throw new ApiError("Video Not deleted(likes)", 500);
+
+  // delete likes of video
+  const isCommentsDeleted = await Comment.deleteMany({
+    video: ObjectId(video?._id),
+  });
+  if (!isCommentsDeleted?.ok)
+    throw new ApiError("Video Not deleted(comments)", 500);
+
+  // delete video
+  const deletedVideo = await Video.findByIdAndDelete(videoId);
+  if (!deletedVideo) throw new ApiError("Video Not deleted(video)", 500);
+
+  // delete video from cloudinary and thumbnail
+  const cloudinaryVideoDeletion = await deleteCloudinaryFile(
+    extractPublicId(deletedVideo.videoFile)
+  );
+  if (!cloudinaryVideoDeletion)
+    throw new ApiError("Video Not deleted(cloudinary)", 500);
+
+  const cloudinaryThumbnailDeletion = await deleteCloudinaryFile(
+    extractPublicId(deletedVideo.thumbnail)
+  );
+  if (!cloudinaryThumbnailDeletion)
+    throw new ApiError("Video Not deleted(cloudinary thumbnail)", 500);
+
+  // return
+  return res
+    .status(202)
+    .json(new ApiRes(202, "Video deleted success", deletedVideo));
 });
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
+
+  if (!videoId) throw new ApiError("Video id not found", 400);
+
+  // check video
+  const video = await Video.findById(videoId);
+  if (!video) throw new ApiError("Video not found", 404);
+
+  // check auth
+  if (video.owner.toString() != req.user?._id.toString())
+    throw new ApiError("User not authourised for this Video", 400);
+
+  // toggling publish status
+  video.isPublished = !video.isPublished;
+  const updatedVideo = await video.save({ validateBeforeSave: false });
+  if (!updatedVideo) throw new ApiError("Video not updated", 500);
+
+  // return
+  return res.status(202).json(new ApiRes(202, "Video publish Toggled success"));
 });
 
 export {
